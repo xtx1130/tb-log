74e7d53053ae36ffa3688544bbd610cb65c1ec6d
xietianxin <xietianxin@baidu.com>
2018-04-03 10:54:00 +0800

    main: add return val

--- index.js
+++ index.js
@@ -16,28 +16,33 @@ class Log {
   }
   warn(str){
     this[output](str, info.warn)
+    return info.warn.level
   }
   error(str){
     this[output](str, info.error)
+    return info.error.level
   }
   success(str) {
     this[output](str, info.success)
+    return info.success.level
   }
   default(str) {
     this[output](str, info.default)
+    return info.default.level
   }
   info(str) {
     this[output](str, info.info)
+    return info.info.level
   }
   start(str) {
     this[output](str, info.start)
+    return info.start.level
   }
   clear () {
     this.stream.write('\x1b[2J\x1b[0f')
   }
   [output](str, info) {
     let baseInfo = pad(`[${this.name} ${info.name}]:`)
-    console.log(baseInfo)
     this.stream.write(chalk[info.color](`${info.icon} ${date()} ${baseInfo}${str}\n`))
   }
 }
