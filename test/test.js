3121ae1443247ccbfaef485151482122b72d78d7
xietianxin <xietianxin@baidu.com>
2018-04-03 10:54:11 +0800

    test: add test case

--- test/test.js
+++ test/test.js
@@ -1,13 +1,27 @@
 'use strict'
 
 const log = require('../')
-
 const Log = require('../').Log
 
-let logT = new Log('xtx')
-logT.warn('1111')
-logT.info('1111')
-log.success('1111')
-log.error('1111')
-log.default('1111')
-log.start('1111')
+let logT = new Log('',false)
+
+describe('Test for log async stdout', () => {
+  it('Test case should be show correctly', () => {
+    expect(log.warn('test case') === 1).toBe(true)
+    expect(log.start('test case') === 2).toBe(true)
+    expect(log.default('test case') === 2).toBe(true)
+  })
+})
+
+describe('Test for log sync stdout', () => {
+  it('Test case should be show correctly', () => {
+    expect(logT.error('test case') === 0).toBe(true)
+    expect(logT.info('test case') === 2).toBe(true)
+    expect(logT.success('test case') === 'none').toBe(true)
+  })
+})
+describe('Test for clear tty', () => {
+  it('Test case should clear tty', () => {
+    expect(log.clear()).toBe(undefined)
+  })
+})
